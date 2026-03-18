edf0e3c6a65a514a855247ea0a82a11a595b5650
Steven Raimondi <traveltoaiur@gmail.com>
2026-03-18 11:11:42 +0100

    feat: implement mouse click and wheel handling across all modes

--- internal/tui/app.go
+++ internal/tui/app.go
@@ -174,11 +174,155 @@ func (a App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		a.statusBar.ClearMessage()
 		return a, nil
 
+	case tea.MouseClickMsg:
+		if msg.Button == tea.MouseLeft {
+			return a.handleMouseClick(msg)
+		}
+		return a, nil
+
+	case tea.MouseWheelMsg:
+		return a.handleMouseWheel(msg), nil
+
 	case tea.KeyPressMsg:
 		return a.handleKey(msg)
 	}
 
 	return a, nil
+}
+
+func (a App) handleMouseClick(msg tea.MouseClickMsg) (tea.Model, tea.Cmd) {
+	switch a.mode {
+	case ModeNormal, ModeSearching:
+		return a.handleNormalMouseClick(msg), nil
+	case ModeComparing, ModeEditingCompare:
+		return a.handleCompareMouseClick(msg), nil
+	case ModeCompareSelect:
+		return a.handleCompareSelectMouseClick(msg), nil
+	case ModeMatrix:
+		return a.handleMatrixMouseClick(msg), nil
+	case ModeHelp:
+		a.mode = ModeNormal
+		return a, nil
+	}
+	return a, nil
+}
+
+func (a App) handleNormalMouseClick(msg tea.MouseClickMsg) App {
+	if msg.X < a.fileWidth {
+		// Click on file list panel
+		a.focus = FocusFiles
+		a.fileList.Focused = true
+		a.varList.Focused = false
+		index := msg.Y - 2 + a.fileList.Offset
+		a.fileList.SetCursor(index)
+		if f := a.fileList.SelectedFile(); f != nil {
+			a.varList.SetFile(f)
+		}
+	} else {
+		// Click on var list panel
+		a.focus = FocusVars
+		a.fileList.Focused = false
+		a.varList.Focused = true
+		index := msg.Y - 2 + a.varList.Offset
+		if index < a.varList.DisplayCount() {
+			a.varList.SetCursor(index)
+		}
+	}
+	return a
+}
+
+func (a App) handleCompareMouseClick(msg tea.MouseClickMsg) App {
+	index := msg.Y - 3 + a.diffView.Offset
+	a.diffView.SetCursor(index)
+	return a
+}
+
+func (a App) handleCompareSelectMouseClick(msg tea.MouseClickMsg) App {
+	index := msg.Y - 2 + a.fileList.Offset
+	a.fileList.SetCursor(index)
+	return a
+}
+
+func (a App) handleMatrixMouseClick(msg tea.MouseClickMsg) App {
+	keyWidth := a.matrixView.layout.MatrixKeyWidth
+	colWidth := a.matrixView.layout.MatrixColWidth
+	row := msg.Y - 2 + a.matrixView.offsetRow
+	if msg.X >= keyWidth {
+		col := (msg.X-keyWidth)/colWidth + a.matrixView.offsetCol
+		a.matrixView.SetCursor(row, col)
+	} else {
+		a.matrixView.SetCursor(row, a.matrixView.cursorCol)
+	}
+	return a
+}
+
+const mouseScrollLines = 3
+
+func (a App) handleMouseWheel(msg tea.MouseWheelMsg) App {
+	up := msg.Button == tea.MouseWheelUp
+	switch a.mode {
+	case ModeNormal, ModeSearching:
+		if msg.X < a.fileWidth {
+			a.scrollFileList(up)
+		} else {
+			a.scrollVarList(up)
+		}
+	case ModeComparing, ModeEditingCompare:
+		a.scrollDiffView(up)
+	case ModeCompareSelect:
+		a.scrollFileList(up)
+	case ModeMatrix, ModeMatrixEditing:
+		a.scrollMatrix(up)
+	}
+	return a
+}
+
+func (a App) scrollFileList(up bool) App {
+	for range mouseScrollLines {
+		if up {
+			a.fileList.MoveUp()
+		} else {
+			a.fileList.MoveDown()
+		}
+	}
+	a.fileList.Select()
+	if f := a.fileList.SelectedFile(); f != nil {
+		a.varList.SetFile(f)
+	}
+	return a
+}
+
+func (a App) scrollVarList(up bool) App {
+	for range mouseScrollLines {
+		if up {
+			a.varList.MoveUp()
+		} else {
+			a.varList.MoveDown()
+		}
+	}
+	return a
+}
+
+func (a App) scrollDiffView(up bool) App {
+	for range mouseScrollLines {
+		if up {
+			a.diffView.MoveUp()
+		} else {
+			a.diffView.MoveDown()
+		}
+	}
+	return a
+}
+
+func (a App) scrollMatrix(up bool) App {
+	for range mouseScrollLines {
+		if up {
+			a.matrixView.MoveUp()
+		} else {
+			a.matrixView.MoveDown()
+		}
+	}
+	return a
 }
 
 func (a App) handleKey(msg tea.KeyPressMsg) (tea.Model, tea.Cmd) {
